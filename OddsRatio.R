884ba7dc06c8ff77a035cfcd943b7d1b6565b076
Zygodon <45967443+Zygodon@users.noreply.github.com>
2019-12-19 21:13:20 +0000

    SELECTING DATA BY 2X2 QUADRAT SIZE, NOT MG*. Added modularity and a first look at communities.Get Pairwise_o_r from file if it exists.

--- OddsRatio.R
+++ OddsRatio.R
@@ -40,7 +40,7 @@ GetTheData <-  function()
                    host   = "sxouse.ddns.net")
   
   
-  q <- sprintf('select assembly_id, assembly_name, quadrat_count, community, quadrat_id, visit_date, records_id, species.species_id, 
+  q <- sprintf('select assembly_id, assembly_name, quadrat_count, community, quadrat_id, quadrat_size, visit_date, records_id, species.species_id, 
     species.species_name from assemblies
       join quadrats on quadrats.assembly_id = assemblies_id
       join visit_dates on quadrats.vd_id = visit_dates.vds_id
@@ -59,37 +59,8 @@ GetTheData <-  function()
 }
 
 OddsRatio <-  function(d, A, B) # the_data, species_name, species_name
-  # Not used yet. Need to check warnings.
 {
-  t <- d %>% filter(species_name %in% c(A, B))
-  As <- t %>% filter(species_name == A)
-  browser()
-  Bs <- t %>% filter(species_name == B)
-  j1 <- full_join(As, Bs, by = "quadrat_id")
-  # get all the quadrats, including ones with neither A nor B
-  q <- d %>% distinct(quadrat_id) 
-  j2 <- (left_join(q, j1, by = "quadrat_id") 
-         # NOTE: column length q >= column length j1
-         %>% mutate(AandB = !is.na(species_name.x) & !is.na(species_name.y))
-         %>% mutate(AnotB = !is.na(species_name.x) & is.na(species_name.y))
-         %>% mutate(BnotA = !is.na(species_name.y) & is.na(species_name.x))
-         %>% mutate(neither = is.na(species_name.x) & is.na(species_name.y)))
-  s <- colSums(j2[,4:7])
-  return((s[1]*s[4])/(s[2]*s[3]))
-}
-
-# End of functions
-
-d <- GetTheData()
-# the_data <- d %>% filter(community == "MG5c")
-the_data <- (d %>% filter(grepl("MG", community))
-             %>% select(quadrat_id, species_name))
-rm(d)
-
-# Odds ratio function
-OddsRatio <-  function(d, A, B) # the_data, species_name, species_name
-{
-  A <- A[1] # Avoiding Warnings
+  A <- A[1]
   B <- B[1]
   t <- d %>% filter(species_name %in% c(A, B))
   As <- t %>% filter(species_name == A)
@@ -104,35 +75,70 @@ OddsRatio <-  function(d, A, B) # the_data, species_name, species_name
          %>% mutate(BnotA = !is.na(species_name.y) & is.na(species_name.x))
          %>% mutate(neither = is.na(species_name.x) & is.na(species_name.y)))
   s <- colSums(j2[,4:7])
-  return((s[1]*s[4])/(s[2]*s[3]))
+  # Standard Error https://en.wikipedia.org/wiki/Odds_ratio
+  se <- sqrt((1/s[1] + (1/s[2]) + (1/s[3]) + (1/s[4]))) # std error of the log(o_r)
+  o_r <- (s[1]*s[4])/(s[2]*s[3])
+  ci_low <- exp(log(o_r)-1.96*se)
+  ci_high <- exp(log(o_r)+1.96*se)
+  retval <- c(o_r, ci_low, ci_high)
+  names(retval) <- c("odds_ratio", "ci_low", "ci_high")
+  return(retval)
 }
 
+# End of functions
 
-# A <- "Achillea_millefolium"
-# B <- "Trifolium_repens" # Pass in as paramters
-# 
-# t <- the_data %>% filter(species_name %in% c(A, B))
-# As <- t %>% filter(species_name == A)
-# Bs <- t %>% filter(species_name == B)
-# j1 <- full_join(As, Bs, by = "quadrat_id")
-# # get all the quadrats, including ones with neither A nor B
-# q <- the_data %>% distinct(quadrat_id) 
-# j2 <- (left_join(q, j1, by = "quadrat_id") 
-#        # NOTE: column length q >= column length j1
-#        %>% mutate(AandB = !is.na(species_name.x) & !is.na(species_name.y))
-#        %>% mutate(AnotB = !is.na(species_name.x) & is.na(species_name.y))
-#        %>% mutate(BnotA = !is.na(species_name.y) & is.na(species_name.x))
-#        %>% mutate(neither = is.na(species_name.x) & is.na(species_name.y)))
-# s <- colSums(j2[,4:7])
-# odds <- (s[1]*s[4])/(s[2]*s[3])
+d <- GetTheData()
+the_data <- (d %>% filter(quadrat_size == "2x2")
+             %>% select(quadrat_id, species_name))
+rm(d)
 
 edges <- (full_join(the_data, the_data, by = "quadrat_id")
-  %>% rename(from = species_name.x)
-  %>% rename(to = species_name.y)
-  %>% filter(from != to))
+           %>% rename(from = species_name.x)
+           %>% rename(to = species_name.y)
+           %>%  select(-quadrat_id)
+           %>% group_by(from, to) 
+           %>% summarise(wt = n())
+           %>% filter(from != to))
+nodes <- distinct(edges, from)
+net <- graph_from_data_frame(d = edges, vertices = nodes, directed = F)
+n2 <- simplify(net, edge.attr.comb = "ignore")
+edges2 <- as_data_frame(n2, what="edges")
 
-# Call OddsRatio function
-odds_ratios <- (head(edges, 24) 
-                %>% group_by(from, to) 
-                %>% summarise(odds_ratio = OddsRatio(the_data, from, to)))
+# It's much quicker to read the file than to compute from scratch
+if (file.exists("Pairwise_Odds_Ratio.csv"))
+{
+  pairwise_o_r <- read.csv("Pairwise_Odds_Ratio.csv") 
+} else
+{
+  n <- length(row.names(edges2))
+  pairwise_o_r <- tibble(
+    odds_ratio = 1:n,
+    ci_low = 1:n,
+    ci_high = 1:n)
+  for (i in seq_along(row.names(edges2)))
+  {
+    pairwise_o_r[i,1:3] <- OddsRatio(the_data, edges2$from[i], edges2$to[i])
+  }
+}
+
+edges3 <-( bind_cols(edges2, pairwise_o_r)
+           %>% filter(!is.infinite(odds_ratio))
+           %>% filter(!is.infinite(ci_low))
+           %>% filter(!is.infinite(ci_high))
+           %>% filter(!is.na(ci_low))
+           %>% filter(!is.na(ci_high))
+           %>% filter(ci_low > 1))
+net2 <- graph_from_data_frame(d = edges3, vertices = nodes, directed = F)
+l <- layout.fruchterman.reingold(net2)
+plot(net2, layout=l, vertex.size = 2, vertex.label=NA)
+
+# Remove isolated vertices (species)
+isolated = which(degree(net2)==0)
+net3 = delete.vertices(net2, isolated)
+l2 = l[-isolated,]
+plot(net3, layout=l2, vertex.size = 5,vertex.label=NA)
+
+wc <- cluster_walktrap(net3)
+modularity(wc)
+plot(wc, net3, layout = l2, vertex.label=NA)
 
