32f23afef94e9a5898bfffdff20ca90863b310ae
Zygodon <45967443+Zygodon@users.noreply.github.com>
2020-02-08 20:28:54 +0000

    Deletions; also Kill local pairings.R updated in many ways incl data tables of friends and enemies.

--- Kill local pairings.R
+++ Kill local pairings.R
@@ -26,6 +26,7 @@
 library(tidyverse)
 library(plotly)
 library(ggpmisc)
+library(DT)
 
 # Functions
 
@@ -91,8 +92,11 @@ QuadratORGivenAssemblyOR <- function(r, marg_x, marg_y, sim_length = 10000, pool
   quads <- quads %>% mutate(X0Y0 = !X & !Y)
   # Calculate the expected quadrat odds ratios given the observed assembly
   # odds ratios but with no local effects
-  csums <- colSums(quads[5:8])
-  quad_or <- log((csums[1]*csums[4])/(csums[2]*csums[3]))
+  s <- colSums(quads[5:8])
+  quad_or <- log((s[1]*s[4])/(s[2]*s[3]))
+  se <- sqrt((1/s[1] + (1/s[2]) + (1/s[3]) + (1/s[4]))) # std error of the log(o_r)
+  q_ci_low <- quad_or - 1.96*se
+  q_ci_high <- quad_or + 1.96*se
   
   # Calculate the assembly OR: have we retained it?
   ass_x <-  quads %>% group_by(ass) %>% summarise(max(X))
@@ -106,7 +110,7 @@ QuadratORGivenAssemblyOR <- function(r, marg_x, marg_y, sim_length = 10000, pool
               %>% select(X1Y1, X1Y0, X0Y1, X0Y0))
   csums <- colSums(ass)
   ass_or <- log((csums[1]*csums[4])/(csums[2]*csums[3]))
-  return (list(quad_or, ass_or))
+  return (list(quad_or, ass_or, q_ci_low, q_ci_high))
 } # end function
 
 
@@ -115,19 +119,20 @@ QuadratORGivenAssemblyOR <- function(r, marg_x, marg_y, sim_length = 10000, pool
 
 pwor <- read.csv("contingency.csv", header = TRUE, sep = ",")
 
-# tibble to hold simulation results
-expected_qor <- tibble(
-  A = pwor$A,
-  B = pwor$B,
-  share_2x2 = pwor$share_2x2,
-  obs_q = pwor$qor,
-  obs_a = pwor$aor,
-  expected_q = 0.0,
-  expected_a = 0.0,
-  sfx = pwor$sfx)
+# pwor columns to hold simulation results
+# expected_qor <- tibble(
+#   A = pwor$A,
+#   B = pwor$B,
+#   share_2x2 = pwor$share_2x2,
+#   obs_q = pwor$qor,
+#   obs_a = pwor$aor,
+  pwor$sim_q = 0.0
+  pwor$sim_a = 0.0
+  pwor$sim_q_low = 0.0
+  pwor$sim_q_high = 0.0
 
 # Simulate OR expected if there were no local effect
-for (i in seq_along(row.names(expected_qor)))
+for (i in seq_along(row.names(pwor)))
 {
   s <- sum(pwor[i, 10:13])
   # assembly marginal probabilities
@@ -136,15 +141,22 @@ for (i in seq_along(row.names(expected_qor)))
   simulated_or <- try(QuadratORGivenAssemblyOR(pwor$aor[i], 
                       amx, amy, sim_length = 2000)) #2000))
   # Don't understand why as.numeric needed here.
-  expected_qor[i,6] <- as.numeric(simulated_or[1]) # Destroyed quadrat ORs
-  expected_qor[i,7] <- as.numeric(simulated_or[2]) # Reconstructed assembly ORs
-  cat("iteration ", i, "\n")
+  # expected_qor[i,6] <- as.numeric(simulated_or[1]) # Destroyed quadrat ORs
+  # expected_qor[i,7] <- as.numeric(simulated_or[2]) # Reconstructed assembly ORs
+  # expected_qor[i,8] <- as.numeric(simulated_or[3]) #
+  # expected_qor[i,9] <- as.numeric(simulated_or[4])  #
+  pwor$sim_q[i] <- as.numeric(simulated_or[1]) # Destroyed quadrat ORs
+  pwor$sim_a[i] <- as.numeric(simulated_or[2]) # Reconstructed assembly ORs
+  pwor$sim_q_low[i] <- as.numeric(simulated_or[3]) #
+  pwor$sim_q_high[i] <- as.numeric(simulated_or[4])  #
+  cat("pair ", i, "\n")
 }
-expected_qor <- expected_qor %>% filter(!is.na(expected_q))
+pwor <- pwor %>% filter(!is.na(sim_q))
 
-# Plot simulated vs observed qor : expect low correlation
+# Plot simulated vs observed qor : expect near zero slope if
+# local influence in strength of association was important.
 lin_model <- y ~ x # For stat_poly_eq
-plt4 <- ggplot(expected_qor, aes(x=obs_q, y=expected_q)) +
+plt4 <- ggplot(pwor, aes(x = qor, y = sim_q)) +
   geom_abline(colour = "green")+
   geom_smooth(method = lm, size = 0.5) +
   stat_poly_eq(formula = lin_model,
@@ -158,7 +170,37 @@ plt4 <- ggplot(expected_qor, aes(x=obs_q, y=expected_q)) +
   theme_grey() + coord_cartesian(xlim = c(-2, 3), ylim = c(-2,3))
 plot(plt4)
 
+# Plot observed quadrat OR ~ assembly OR, superimpose reconstructed
+# quadrat OR
+plt5 <- ggplot(pwor, aes(x=aor, y=qor)) +
+  geom_smooth(method = lm) +
+  geom_errorbar(aes(ymin = ci_low.x, ymax = ci_high.x), 
+                size = 0.1, width = 0.1, colour = "green", alpha = 0.5) +
+  geom_errorbar(aes(ymin = sim_q_low, ymax = sim_q_high), 
+                size = 0.1, width = 0.1, colour = "black", alpha = 0.5) +
+  geom_point(aes(x = pwor$aor, y = pwor$sim_q,
+             text = paste(pwor$A, pwor$B, sep=",")), shape = 1, 
+             colour = "olivedrab3", alpha = 0.5) +
+  # geom_point(aes(colour = sfx, text = paste(A, B, sep=",")), alpha = 0.5) +
+  # scale_colour_manual(values = c("grey27", "sienna3")) +
+  geom_point(aes(colour = share_2x2, text = paste(A, B, sep=",")), alpha = 0.5) +
+  scale_colour_gradient(low = "sienna1", high = "black") +
+  labs(x = "log(Odds Ratio), assemblies", y = "log(Odds Ratio), quadrats") +
+  theme_grey() + coord_cartesian(xlim = c(min(pwor$aor), max(pwor$qor)), 
+                                 ylim = c(min(pwor$aor), max(pwor$qor)))
+plotly::ggplotly(plt5) # 
+
+friends <- datatable(pwor %>% filter((sim_q_high < ci_low.x) & sfx == "yes")
+            %>% select(A, B, qor, share_2x2), 
+            caption = 'Plants found sharing a quadrat',
+            colnames = c('log(odds ratio)' = 4, 'shared quadrats' = 5))
+friends <- friends %>%  formatSignif('log(odds ratio)',2)
+
+enemies <- datatable(pwor %>% filter((sim_q_low > ci_high.x) & (sfx == "yes") & (qor < 0))
+            %>% select(A, B, qor, share_2x2),
+            caption = 'Plants that avoid sharing a quadrat',
+            colnames = c('log(odds ratio)' = 4, 'shared quadrats' = 5))
+enemies <- enemies %>% formatSignif('log(odds ratio)',2)
 
 # write.csv(expected_qor, "predicted assembly_OR with no quadrat_OR.csv", row.names = FALSE)
-# 
 
