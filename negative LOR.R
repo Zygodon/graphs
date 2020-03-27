5a9458b78670654881fd18f69e28e215cbd9187b
Zygodon <45967443+Zygodon@users.noreply.github.com>
2020-03-27 10:50:27 +0000

    First commit. Using tidygraph; including negative LOR

---
+++ negative LOR.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(igraph)
+library(ggraph)
+library(tidygraph)
+library("colorspace")
+
+site_occ <- read_csv("stand occupancy.csv")
+edges <- read_csv("edges.csv")
+# Get a vertices list
+edge_pivot <- edges %>% select(A, B) %>% pivot_longer(cols = c(A,B), names_to = "origin", values_to = "species")
+vertices <- edge_pivot %>% group_by(species) %>% summarise(count = n()) %>% select(-count) #nodes; vertices
+rm(edge_pivot)
+
+G0 <- (tbl_graph(nodes = vertices, edges = edges)
+       %>% activate(edges)
+       %>% filter(p_val < 0.05)
+       %>% filter(!(from == to))
+       # %>% filter(lor > 0)
+       %>% mutate(weight = lor)
+       %>% activate(nodes)
+       %>% mutate(degree = degree(., V(.)))
+       %>% filter(degree > 5))
+      #  %>% mutate(community = as.factor(group_infomap())))
+
+#
+# G0 <- (G0 %>% activate(edges)
+#       %>% mutate(community = ifelse(.N()$community[from] == .N()$community[to], .N()$community[from], NA)))
+# eg0 <- G0 %>% activate(edges) %>% as_tibble(.)
+# no0 <- G0 %>% activate(nodes) %>% as_tibble(.)
+
+# lo <- layout_with_fr(G0)
+
+#G0 %>% ggraph(layout = lo) + 
+G0 %>% ggraph(layout = 'fr') + 
+  # geom_edge_link(aes(colour = as.factor(community)), alpha = 0.6, show.legend = F) + 
+  # scale_edge_colour_brewer(palette = "Set1", na.value = "grey50",) +
+  geom_edge_link(aes(colour = ifelse(lor > 0, "positive", "negative")), alpha = 0.6) + 
+  # geom_node_point(aes(colour = (community), size = degree), alpha = 0.8) + 
+  geom_node_point(aes(size = degree), alpha = 0.8) + 
+  # scale_colour_brewer(palette = "Set1", na.value = "grey50",) +
+  ggtitle('G0') 
+
