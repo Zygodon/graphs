5b2877b92e259ce3e2c28616261154b9e5805a1e
Zygodon <45967443+Zygodon@users.noreply.github.com>
2020-03-27 20:30:45 +0000

    Positive LOR.R clusters use negative LOR as well as positive LOR

--- negative LOR.R
+++ negative LOR.R
@@ -15,28 +15,23 @@ G0 <- (tbl_graph(nodes = vertices, edges = edges)
        %>% activate(edges)
        %>% filter(p_val < 0.05)
        %>% filter(!(from == to))
-       # %>% filter(lor > 0)
+       %>% filter(lor < 0)
        %>% mutate(weight = lor)
        %>% activate(nodes)
        %>% mutate(degree = degree(., V(.)))
        %>% filter(degree > 5))
-      #  %>% mutate(community = as.factor(group_infomap())))
+       # %>% activate(edges)
+       # %>% filter(p_val < 0.05)
+       # %>% filter(!(from == to))
+       # %>% filter(lor < 0)
+       # %>% mutate(weight = lor))
 
-#
-# G0 <- (G0 %>% activate(edges)
-#       %>% mutate(community = ifelse(.N()$community[from] == .N()$community[to], .N()$community[from], NA)))
-# eg0 <- G0 %>% activate(edges) %>% as_tibble(.)
-# no0 <- G0 %>% activate(nodes) %>% as_tibble(.)
+lo <- layout_with_fr(G0)
 
-# lo <- layout_with_fr(G0)
-
-#G0 %>% ggraph(layout = lo) + 
-G0 %>% ggraph(layout = 'fr') + 
-  # geom_edge_link(aes(colour = as.factor(community)), alpha = 0.6, show.legend = F) + 
-  # scale_edge_colour_brewer(palette = "Set1", na.value = "grey50",) +
-  geom_edge_link(aes(colour = ifelse(lor > 0, "positive", "negative")), alpha = 0.6) + 
-  # geom_node_point(aes(colour = (community), size = degree), alpha = 0.8) + 
+G0 %>% ggraph(layout = lo) + 
+#G0 %>% ggraph(layout = 'fr') + 
+  geom_edge_link(colour = "red", alpha = 0.6) + 
   geom_node_point(aes(size = degree), alpha = 0.8) + 
-  # scale_colour_brewer(palette = "Set1", na.value = "grey50",) +
+  # geom_node_text(aes(label=species), repel=TRUE, size =1.5,colour="black") +
   ggtitle('G0') 
 
--- positive LOR.R
+++ positive LOR.R
@@ -4,13 +4,14 @@ library(ggraph)
 library(tidygraph)
 library("colorspace")
 
-site_occ <- read_csv("stand occupancy.csv")
+# site_occ <- read_csv("stand occupancy.csv")
 edges <- read_csv("edges.csv")
 # Get a vertices list
 edge_pivot <- edges %>% select(A, B) %>% pivot_longer(cols = c(A,B), names_to = "origin", values_to = "species")
 vertices <- edge_pivot %>% group_by(species) %>% summarise(count = n()) %>% select(-count) #nodes; vertices
 rm(edge_pivot)
 
+# G0: positive LOR only
 G0 <- (tbl_graph(nodes = vertices, edges = edges)
        %>% activate(edges)
        %>% filter(p_val < 0.05)
@@ -36,3 +37,44 @@ G0 %>% ggraph(layout = lo) +
   scale_colour_brewer(palette = "Set1", na.value = "grey50",) +
   ggtitle('G0') 
 
+# G1: Both positive and negative LOR, weights OR
+# make a continuous palette
+newcol <- colorRampPalette(brewer.pal(9, "BrBG"))
+ncols <- 100
+BrBG <- newcol(ncols)#apply the function to get 100 colours
+rm(newcol, ncols)
+
+mu <- mean(edges$lor)
+sigma <- sd(edges$lor)
+
+G1 <- (tbl_graph(nodes = vertices, edges = edges)
+       %>% activate(edges)
+       %>% filter(p_val < 0.05)
+       %>% filter(!(from == to))
+       %>% mutate(weight = pnorm(lor, mean = mu, sd = sigma))
+       %>% activate(nodes)
+       %>% mutate(degree = degree(., V(.)))
+       %>% filter(degree > 5))
+       
+im <- cluster_infomap(G1)
+G1 <-(G1 %>% activate(nodes) 
+      %>% mutate(community = as.factor(membership(im))))
+
+eg1 <- G1 %>% activate(edges) %>% as_tibble(.)
+no1 <- G1 %>% activate(nodes) %>% as_tibble(.)
+
+G1 %>% ggraph(layout = "fr") + 
+  geom_edge_link(aes(colour = lor), alpha = 1, show.legend = T) +
+  scale_edge_color_gradient(low = "grey80", high = "tan4") +
+  geom_node_point(aes(colour = (community), size = degree), alpha = 0.8) +
+  scale_colour_brewer(palette = "Set1", na.value = "grey50",) +
+  ggtitle('G1') 
+
+# Attempt to remove small modules
+G2 <- G1 %>% activate(nodes) %>% filter(as.numeric(community) <= 4)
+G2 %>% ggraph(layout = "fr") + 
+  geom_edge_link(aes(colour = lor), alpha = 1, show.legend = T) +
+  scale_edge_color_gradient(low = "yellow", high = "blue") +
+  geom_node_point(aes(colour = (community), size = degree), alpha = 0.8) +
+  scale_colour_brewer(palette = "Set1", na.value = "grey50",) +
+  ggtitle('G1') 
