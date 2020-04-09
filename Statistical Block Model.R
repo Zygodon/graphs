f2d0b4dfa13c9cccb0707e326e277ad05a46e8b4
Zygodon <45967443+Zygodon@users.noreply.github.com>
2020-04-09 20:56:31 +0100

    Species names on adjacency matrix plot

--- Statistical Block Model.R
+++ Statistical Block Model.R
@@ -11,7 +11,10 @@ library(blockmodels)
 edges <- read_csv("edges.csv")
 # Get a vertices list
 edge_pivot <- edges %>% select(A, B) %>% pivot_longer(cols = c(A,B), names_to = "origin", values_to = "species")
-vertices <- edge_pivot %>% group_by(species) %>% summarise(count = n()) %>% select(-count) #nodes; vertices
+vertices <- (edge_pivot %>% group_by(species) 
+             %>% summarise(count = n()) 
+             %>% select(-count) #nodes; vertices
+             %>% rename(name = species))
 rm(edge_pivot)
 
 # G0: positive LOR only
@@ -49,16 +52,6 @@ ggraph(G0, 'matrix', sort.by = node_rank_leafsort()) +
 # Sort nodes by SBM community prior to looking at block matrix
 G1 <- G0 %>% activate(nodes) %>% arrange(sbm_comm)
 sp <- V(G1)$species
-
-ggraph(G1, 'matrix', sort.by = node_rank_visual()) + 
-  geom_edge_point(aes(colour = as.factor(sbm_comm)), mirror = TRUE) + 
-  scale_y_discrete(label = layout$species) +
-  # theme(legend.position = 'bottom') 
-  theme_bw() + theme(axis.text.y = element_text(size = 0.1))
-
-n1 <- as_tibble(G1 %>% activate(nodes)) 
-  
-
 lo <- layout_with_fr(G1)
 G1 %>% activate(edges) %>% ggraph(layout = lo) + 
   geom_edge_link(colour = "black", alpha = 0.2) +
@@ -67,6 +60,25 @@ G1 %>% activate(edges) %>% ggraph(layout = lo) +
   ggtitle('G0 SBM Bernouilli, no weights, positive LOR') +
   theme_minimal() + th_no_axes( ) +
   guides(fill = guide_legend(override.aes = list(size=5))) # +
+
+# This plot shows edges between communities as NA
+# But I cannot get species names in margin
+ggraph(G1, 'matrix', sort.by = node_rank_visual()) + 
+  geom_edge_point(aes(colour = as.factor(sbm_comm)), mirror = TRUE) +
+  scale_y_reverse(breaks = seq(1, 79, by = 1), labels = no1$name, "to") +
+  scale_x_continuous(breaks = seq(1, 79, by = 1), labels = no1$name, "from") +
+  coord_fixed() +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 4.0, angle = 90)) +
+  theme(axis.text.y = element_text(size = 4.0)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+  #theme(panel.background = element_rect(fill = "white", colour = "white"))
+
+ggsave("SBM_1.jpg", width = 20, height = 20, units = "cm")
+
+  
+
 
 
 # cle <- cluster_leading_eigen(G0, weights = NA)
