a9ee5567a77d908e0db59d6fa8dfb1d2cc8daa41
Zygodon <45967443+Zygodon@users.noreply.github.com>
2020-04-13 20:21:46 +0100

    first commit

---
+++ Poisson SBM_1.R
@@ -0,0 +1,127 @@
+library("RMySQL")
+library(tidyverse)
+library(igraph)
+library(ggraph)
+library(tidygraph)
+library(RColorBrewer)
+library(DT)
+library(blockmodels)
+
+# Functions
+dbDisconnectAll <- function(){
+  ile <- length(dbListConnections(MySQL())  )
+  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
+  cat(sprintf("%s connection(s) closed.\n", ile))
+}
+
+# General SQL query
+query <- function(q)
+{
+  # Remote DB with password
+  con <- dbConnect(MySQL(), 
+                   user  = "guest",
+                   password    = "guest",
+                   dbname="meadows",
+                   port = 3306,
+                   host   = "sxouse.ddns.net")
+  rs1 = dbSendQuery(con, q)
+  return(as_tibble(fetch(rs1, n=-1)))
+  dbDisconnectAll()
+}
+
+# Load the database
+GetTheData <-  function()
+{
+  # GET DATA FROM DB
+  # Remote DB with password
+  con <- dbConnect(MySQL(), 
+                   user  = "guest",
+                   password    = "guest",
+                   dbname="meadows",
+                   port = 3306,
+                   host   = "sxouse.ddns.net")
+  
+  
+  q <- sprintf('select assembly_id, assembly_name, quadrat_count, community, quadrat_id, quadrat_size, visit_date, records_id, species.species_id, 
+    species.species_name from assemblies
+      join quadrats on quadrats.assembly_id = assemblies_id
+      join visit_dates on quadrats.vd_id = visit_dates.vds_id
+      join records on records.quadrat_id = quadrats_id
+      join species on species.species_id = records.species_id
+      # Two assemblies have 0 quadrat count; exclude A.capillaris_stolonifera;
+      # exclude some odd assemblies with no assigned community
+    where quadrat_count = 5 and species.species_id != 4 and community is not null
+    and quadrat_size = "2x2";') 
+  # NOTE: this extract includes "MG5", i.e. some MG5 communities where 
+  # the team have not decided
+  # on a sub-group.
+  
+  rs1 = dbSendQuery(con, q)
+  return(as_tibble(fetch(rs1, n=-1)))
+  dbDisconnectAll()
+}
+
+the_data <- GetTheData()
+
+# Make the stand (assembly) based occupancy matrix site_occ. Start with d2:
+d2 <- (the_data %>% select(assembly_id, species_name)
+       %>% group_by(assembly_id, species_name)
+       %>% summarise(n=n())
+       %>% ungroup()
+       %>% pivot_wider(names_from = species_name, values_from = n))
+# At this point, d2 has the number of hits for each assembly and species.
+# Replace anything numeric with 1, and any NA with 0
+d3 <- (d2 %>% select(-assembly_id) %>% replace(., !is.na(.), 1)
+       %>% replace(., is.na(.), 0)) # Replace NAs with 0)
+# Insert a column with the stand IDs. I know that it is usual with occupancy
+# matrices to have the species as rows and sites as columns but as I intend
+# analysis by species (R analysis as opposed to Q) I find it easier for
+# the moment to think of the species in columns.
+cn <- colnames(d3)
+d3 <- (mutate(d3, stand = paste("S", as.character(d2$assembly_id), sep = "_"))
+       %>% select(stand, cn)) #Re order columns so stand 1st
+# Rename and delete d3
+site_occ <- d3
+rm(d2, d3)
+
+# Pairwise list of all species combinations (expand.grid useful)
+x <- colnames(site_occ)[2:length(colnames(site_occ))] # Remove the "stand" column name
+e0 <- as_tibble(expand.grid(x, x))
+# e0 Includes B -> A as well as A -> B
+# Make a graph and simplify it to remove multiple edges
+G <- graph_from_data_frame(d = e0, vertices = x, directed = F)
+G <- simplify(G, edge.attr.comb = "first")
+edges <- as_tibble(as_data_frame(G, what="edges"))
+nodes <- as_tibble(as_data_frame(G, what="vertices"))
+# rm(x, e0, G)
+
+edges$a <- 0
+
+pb <- txtProgressBar(min=0, max=length(row.names(edges)))
+
+for (i in seq_along(row.names(edges)))
+{
+  e1 <- as.character(edges$from[i]) # First species name
+  e2 <- as.character(edges$to[i]) # Second species name
+  jc <- (site_occ %>% select(e1, e2) # Two vectors, 108 long
+         %>% rename(from = 1, to = 2))
+  t <- table(jc$from, jc$to)
+  if((0 %in% t) | sum(dim(t)) < 4){
+    edges$a[i] <- 0 #NA
+  } else {
+    edges$a[i] <- t[2,2]
+  }
+  setTxtProgressBar(pb, i)
+} # end for
+close(pb)
+
+rm(i, t, e1, e2, jc, pb)
+
+G0 <- graph_from_data_frame(d = edges, vertices = nodes, directed = F)
+M <- as_adjacency_matrix(G0, type = "both", attr = "a", edges = FALSE, names = F, sparse = F)
+
+# Blockmodel Poisson
+my_model <- BM_poisson("SBM",M )
+my_model$estimate()
+which.max(my_model$ICL)
+
