d39557cb806f07b829bc1ba36461c1dd5a498be7
Zygodon <45967443+Zygodon@users.noreply.github.com>
2020-05-23 19:54:06 +0100

    Extract domin values, mean by species.

---
+++ Species domin.R
@@ -0,0 +1,67 @@
+library("RMySQL")
+library(tidyverse)
+library(igraph)
+library(vcd)
+
+# Functions
+dbDisconnectAll <- function(){
+  ile <- length(dbListConnections(MySQL())  )
+  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
+  cat(sprintf("%s connection(s) closed.\n", ile))
+}
+
+# General SQL query
+query <- function(q)
+{
+  # Remote DB with password
+  con <- dbConnect(MySQL(), 
+                   user  = "guest",
+                   password    = "guest",
+                   dbname="meadows",
+                   port = 3306,
+                   host   = "sxouse.ddns.net")
+  rs1 = dbSendQuery(con, q)
+  return(as_tibble(fetch(rs1, n=-1)))
+  dbDisconnectAll()
+}
+
+# Load the database
+GetTheData <-  function()
+{
+  # GET DATA FROM DB
+  # Remote DB with password
+  con <- dbConnect(MySQL(), 
+                   user  = "guest",
+                   password    = "guest",
+                   dbname="meadows",
+                   port = 3306,
+                   host   = "sxouse.ddns.net")
+  
+  
+  q <- sprintf('select assembly_id, assembly_name, quadrat_count, community, quadrat_id, quadrat_size, visit_date, records_id, species.species_id, 
+    species.species_name, domin from assemblies
+      join quadrats on quadrats.assembly_id = assemblies_id
+      join visit_dates on quadrats.vd_id = visit_dates.vds_id
+      join records on records.quadrat_id = quadrats_id
+      join species on species.species_id = records.species_id
+      # Two assemblies have 0 quadrat count; exclude A.capillaris_stolonifera;
+      # exclude some odd assemblies with no assigned community
+    where quadrat_count = 5 and species.species_id != 4 and community is not null
+    and quadrat_size = "2x2";') 
+  # NOTE: this extract includes "MG5", i.e. some MG5 communities where 
+  # the team have not decided
+  # on a sub-group.
+  
+  rs1 = dbSendQuery(con, q)
+  return(as_tibble(fetch(rs1, n=-1)))
+  dbDisconnectAll()
+}
+
+the_data <- GetTheData()
+
+d2 <- (the_data %>% select(species_name, domin)
+       %>% group_by(species_name)
+       %>% summarise(d=mean(domin))
+       %>% ungroup())
+
+write.csv(d2, "domin.csv")
